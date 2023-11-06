4d3813c02c6d6744f96fdd1793b49be54f8b65e6
tamerlan-b <tamerlan.baxov@mail.ru>
2023-11-06 14:37:05 +0300

    feat: Add Laplace distribution

--- assignment02_laplace/distribution.py
+++ assignment02_laplace/distribution.py
@@ -11,6 +11,7 @@ class LaplaceDistribution:
         ####
         # Do not change the class outside of this block
         # Your code here
+        pass
         ####
 
     def __init__(self, features):
@@ -20,8 +21,9 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        self.loc = # YOUR CODE HERE
-        self.scale = # YOUR CODE HERE
+        self.loc = np.median(features, axis=0)
+        n = features.shape[0]
+        self.scale = np.sum(np.abs(features - self.loc), axis=0) / n # * n / (n - 2)
         ####
 
 
@@ -33,7 +35,7 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        return 
+        return -(np.divide(np.abs(values - self.loc), self.scale) + np.log(2 * self.scale))
         ####
         
     
@@ -43,4 +45,4 @@ class LaplaceDistribution:
         Args:
             values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
         '''
-        return np.exp(self.logpdf(value))
+        return np.exp(self.logpdf(values))
